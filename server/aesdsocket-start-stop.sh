d9fef5eddff3f9b1a8e0a8049fa46397648f7db0
Juri Kuhn <kuhn@in.tum.de>
2024-09-22 12:26:43 +0000

    fix

--- server/aesdsocket-start-stop.sh
+++ server/aesdsocket-start-stop.sh
@@ -5,7 +5,7 @@ trap 'echo "Exiting .."; kill $(pgrep) aesdsock; exit 0' SIGINT
 
 if [ -e "aesdsock" ] then
     echo "starting aesd sock"
-    ./aesdsock -d
+    aesdsock -d
 else
     echo "aesdsock not found"
     exit(-1)
