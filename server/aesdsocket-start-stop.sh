ec566f633df52bc31d2c0a361264c9f395a4c529
Juri Kuhn <kuhn@in.tum.de>
2024-09-22 11:45:01 +0000

    preparation for 5.2

---
+++ server/aesdsocket-start-stop.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+
+trap 'echo "Exiting .."; kill $(pgrep) aesdsock; exit 0' SIGINT
+
+if [ -e "aesdsock" ] then
+    echo "starting aesd sock"
+    ./aesdsock -d
+else
+    echo "aesdsock not found"
+    exit(-1)
+fi
+
+while true; do
+    sleep 1
+done
